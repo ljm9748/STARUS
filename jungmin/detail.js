eeaecf13e479876c93f500e3feb599eb7f79b5ce
Jungmin <ljm9748@naver.com>
2020-12-09 16:49:35 +0900

    Add js fime

---
+++ jungmin/detail.js
@@ -0,0 +1,13 @@
+$('#').click(function() {
+    alert('click');
+});
+
+function dt_page(menuid) {
+    //메뉴아이디 받기
+    var menuid = menuid;
+    //해당메뉴 그림과 이름 가격 적용
+
+}
+$('#').click(function() {
+    alert('click');
+});
