611efec3611f6d09a8c4bcdf90c6d225a1c3b44a
Jungmin <ljm9748@naver.com>
2020-12-09 17:41:10 +0900

    Add submit on detail.js

--- jungmin/detail.js
+++ jungmin/detail.js
@@ -1,5 +1,15 @@
-$('#').click(function() {
+$(document).ready(function() {
+    $('#dt_refresh').click(function() {
+        alert('replace');
+    });
+
+});
+$("#dt_done").click(function() {
     alert('click');
+
+    var $hotice = $(this).find("#hotice")
+    var hotice = $hotice.value();
+    console.log(hotice);
 });
 
 function dt_page(menuid) {
@@ -7,7 +17,22 @@ function dt_page(menuid) {
     var menuid = menuid;
     //해당메뉴 그림과 이름 가격 적용
 
+
 }
-$('#').click(function() {
-    alert('click');
-});
+
+//장바구니에 담기 버튼 누르면
+function dt_finish() {
+
+
+    var hotice = $(":input:radio[name=hotice]:checked").val();
+    alert(hotice);
+    var size = $(":input:radio[name=size]:checked").val();
+    alert(size);
+
+    if (size == "Tall") var money = 0;
+    else if (size == "Grande") var money = 500;
+    else var money = 1000;
+
+    var string = hotice + "/" + size + "+" + money + "원을 선택하셨습니다.";
+    alert(string);
+}
