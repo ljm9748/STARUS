7da151d8586087f8ed5aaa6a20d3538df261f8dc
Jungmin <ljm9748@naver.com>
2020-12-10 12:52:57 +0900

    Update detail page with data

--- jungmin/detail.js
+++ jungmin/detail.js
@@ -1,4 +1,10 @@
+let nowitem;
 $(document).ready(function() {
+    alert("hi");
+    nowitem = JSON.parse(localStorage.getItem('coffee'))[0];
+    console.log(nowitem);
+    dt_page();
+
     $('#dt_refresh').click(function() {
         alert('replace');
         location.replace('detail.html');
@@ -13,14 +19,15 @@ $(document).ready(function() {
 $("#dt_done").click(function() {
     alert('click');
 
-    var $hotice = $(this).find("#hotice")
-    var hotice = $hotice.value();
+    let $hotice = $(this).find("#hotice")
+    let hotice = $hotice.value();
     console.log(hotice);
 });
 
-function dt_page(menuid) {
-    //메뉴아이디 받기
-    var menuid = menuid;
+function dt_page() {
+
+    $("#dt_menuimg").attr();
+
     //해당메뉴 그림과 이름 가격 적용
 
 
@@ -29,8 +36,8 @@ function dt_page(menuid) {
 //장바구니에 담기 버튼 누르면
 function dt_finish() {
 
-    var hotice = $(":input:radio[name=hotice]:checked").val();
-    var size = $(":input:radio[name=size]:checked").val();
+    let hotice = $(":input:radio[name=hotice]:checked").val();
+    let size = $(":input:radio[name=size]:checked").val();
 
     if (hotice == null) {
         alert("HOT/ICE를 선택해 주세요");
@@ -41,11 +48,11 @@ function dt_finish() {
         return true;
     }
 
-    if (size == "Tall") var money = 0;
-    else if (size == "Grande") var money = 500;
-    else var money = 1000;
+    if (size == "Tall") let money = 0;
+    else if (size == "Grande") let money = 500;
+    else let money = 1000;
 
-    var string = hotice + "/" + size + "+" + money + "원을 선택하셨습니다.";
+    let string = hotice + "/" + size + "+" + money + "원을 선택하셨습니다.";
     alert(string);
 
     $(document).location.replace('../jungeun/menuManager.html');
