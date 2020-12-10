eedae4286ed56ed61202beb894e9f51d41dc585c
KIM <jnghyn93@gmail.com>
2020-12-10 21:21:11 +0900

    pay js

--- kjh/pay.js
+++ kjh/pay.js
@@ -6,67 +6,98 @@ window.onload = function () {
     // 장바구니에서 주문 목록 가져오기
     ordermenu = JSON.parse(localStorage.getItem('buyList'));
 
-    console.log(ordermenu[0]);
-
     var sum = 0;
     for (var i = 0; i < ordermenu.length; i++) {
         sum = sum + Number(ordermenu[i].price);
     }
 
 
-    // 주문 목록 보여주기
-    function showOderlist() {
+    var tbody = '';
 
-        // table ordertbody 캐스팅
+    for (var i = 0; i < ordermenu.length; i++) {
+
         var orderlistrow = document.querySelector('#orderlistrow');
 
-        var tbody = '';
+        if (!ordermenu[i].isdrink) {
+            console.log('음료아님', ordermenu[i]);
 
-        for (var i = 0; i < ordermenu.length; i++) {
+            tbody += '<tr>';
+            tbody += '<td class="otft">';
+            tbody += '<img src="' + ordermenu[i].img + '">';
+            tbody += '<span>' + ordermenu[i].name + '</span>';
+            tbody += '</td>';
+            tbody += '<td class="ottd">' + ordermenu[i].count + '</td>';
+            tbody += '<td class="ottd">' + ordermenu[i].price + '원</td>';
+            tbody += '</tr>';
+            orderlistrow.innerHTML = tbody;
+
+
+        } else {
+            console.log('음료', ordermenu[i]);
+
             tbody += '<tr>';
             tbody += '<td class="otft">';
             tbody += '<img src="' + ordermenu[i].img + '">';
             tbody += '<span>' + ordermenu[i].name + '</span>';
             tbody += '<span>' + ordermenu[i].hotice + '</span>';
+            tbody += '<span>/</span>';
             tbody += '<span>' + ordermenu[i].size + '</span>';
             tbody += '</td>';
-            tbody += '<td class="ottd">1</td>';
+            tbody += '<td class="ottd">' + ordermenu[i].count + '</td>';
             tbody += '<td class="ottd">' + ordermenu[i].price + '원</td>';
             tbody += '</tr>';
+            orderlistrow.innerHTML = tbody;
+
         }
 
-        orderlistrow.innerHTML = tbody;
+
+    }
+
+
+
+    // 주문 목록 보여주기
+    function showOderlist() {
+
+
+
+        //        // 주문 목록
+        //        var orderlistrow = document.querySelector('#orderlistrow');
+        //
+        //        var tbody = '';
+        //
+        //        for (var i = 0; i < ordermenu.length; i++) {
+        //            tbody += '<tr>';
+        //            tbody += '<td class="otft">';
+        //            tbody += '<img src="' + ordermenu[i].img + '">';
+        //            tbody += '<span>' + ordermenu[i].name + '</span>';
+        //            tbody += '<span>' + ordermenu[i].hotice + '</span>';
+        //            tbody += '<span>' + ordermenu[i].size + '</span>';
+        //            tbody += '</td>';
+        //            tbody += '<td class="ottd">'+ordermenu[i].count+'</td>';
+        //            tbody += '<td class="ottd">' + ordermenu[i].price + '원</td>';
+        //            tbody += '</tr>';
+        //        }
+        //
+        //        orderlistrow.innerHTML = tbody;
+
 
     };
-    showOderlist();
 
 
     // 총 금액 보여주기
-    function showPricesum() {
 
-        var odsum = 0;
-        for (var i = 0; i < ordermenu.length; i++) {
-            odsum += Number(ordermenu[i].price);
-        }
+    var odsum = 0;
+    for (var i = 0; i < ordermenu.length; i++) {
+        odsum += Number(ordermenu[i].price);
+    }
 
-        // 캐스팅
-        var pricesum = document.querySelector('#pricesum');
+    // 캐스팅
+    var pricesum = document.querySelector('#pricesum');
 
-        var div = '<span>총 금액 :</span>';
-        div += '<span>' + odsum + '원</span>';
+    var div = '<span>총 금액 :</span>';
+    div += '<span>' + odsum + '원</span>';
 
-        pricesum.innerHTML = div;
-
-    };
-    showPricesum();
+    pricesum.innerHTML = div;
 
 
 };
-
-
-
-
-
-
-
-
